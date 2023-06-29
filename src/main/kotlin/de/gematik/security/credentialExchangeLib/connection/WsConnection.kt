bf8746b68b1d60b01ed261c8be6c12c0e892e725
rk <ronald.koenig@gematik.de>
2023-06-29 17:31:51 +0200

    version 0.1.0 fix: close connections chore: CryptoCredentials

--- src/main/kotlin/de/gematik/security/credentialExchangeLib/connection/WsConnection.kt
+++ src/main/kotlin/de/gematik/security/credentialExchangeLib/connection/WsConnection.kt
@@ -1,10 +1,13 @@
 package de.gematik.security.credentialExchangeLib.connection
 
 import de.gematik.security.credentialExchangeLib.connection.WsConnection.Companion.getConnection
+import de.gematik.security.credentialExchangeLib.json
+import de.gematik.security.credentialExchangeLib.protocols.Close
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.websocket.*
 import io.ktor.http.*
+import io.ktor.serialization.*
 import io.ktor.serialization.kotlinx.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
@@ -13,9 +16,12 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.channels.ClosedReceiveChannelException
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToJsonElement
+import kotlinx.serialization.json.jsonObject
 import mu.KotlinLogging
 import java.util.*
 
@@ -71,6 +77,7 @@ class WsConnection private constructor(private val session: DefaultWebSocketSess
         connections.remove(this.id)
         runBlocking {
             logger.info("close connection: $id")
+            send(Message(json.encodeToJsonElement(Close(message = "connection closed normally")).jsonObject, MessageType.CLOSE))
             session.close(CloseReason(CloseReason.Codes.NORMAL, "Normal"))
         }
     }
@@ -82,37 +89,56 @@ class WsConnection private constructor(private val session: DefaultWebSocketSess
     }
 
     override suspend fun send(message: Message) {
-        logger.info { "send: ${Json.encodeToString(message)}" }
-        when (session) {
-            is DefaultClientWebSocketSession -> session.sendSerialized(message)
-            is DefaultWebSocketServerSession -> session.sendSerialized(message)
-        }
+        logger.info { "send:    ${message.type} ${Json.encodeToString(message)}" }
+        runCatching {
+            when (session) {
+                is DefaultClientWebSocketSession -> session.sendSerialized(message)
+                is DefaultWebSocketServerSession -> session.sendSerialized(message)
+            }
+        }.onFailure { logger.info { it.message } }
     }
 
     private var isFirstReceive = true
     override suspend fun receive(): Message {
-        val message = when (session) {
-            is DefaultClientWebSocketSession -> kotlin.runCatching {
-                session.receiveDeserialized<Message>()
-            }.getOrDefault(Message("connection closed", MessageType.CLOSED))
-            is DefaultWebSocketServerSession -> {
-                if (isFirstReceive && session.call.parameters.contains("oob")) {
-                    Message(
-                        String(Base64.getDecoder().decode(session.call.parameters["oob"])),
-                        MessageType.INVITATION_ACCEPT
-                    )
-                } else {
-                    kotlin.runCatching {
-                        session.receiveDeserialized<Message>()
-                    }.getOrDefault(Message("connection closed", MessageType.CLOSED))
-                }
-            }
+        while(true) {
+            val message = when (session) {
+                is DefaultClientWebSocketSession -> kotlin.runCatching {
+                    session.receiveDeserialized<Message>()
+                }.onFailure {
+                    when(it){
+                        is WebsocketDeserializeException -> logger.debug { it.frame.frameType.name }
+                        is ClosedReceiveChannelException -> {
+                            logger.debug { "connection closed by remote peer unexpectly" }
+                            return Message(json.encodeToJsonElement(Close(message = "connection closed by remote peer unexpectly")).jsonObject, MessageType.CLOSE)
+                        }
+                        else -> logger.debug { it.message }
+                    }
+                }.getOrNull()
 
-            else -> throw IllegalStateException("wrong session type")
+                is DefaultWebSocketServerSession -> {
+                    if (isFirstReceive && session.call.parameters.contains("oob")) {
+                        Message(
+                            json.parseToJsonElement(
+                                String(
+                                    Base64.getDecoder().decode(session.call.parameters["oob"])
+                                )
+                            ).jsonObject,
+                            MessageType.INVITATION_ACCEPT
+                        )
+                    } else {
+                        kotlin.runCatching {
+                            session.receiveDeserialized<Message>()
+                        }.onFailure { logger.debug { it.message } }.getOrNull()
+                    }
+                }
+
+                else -> throw IllegalStateException("wrong session type")
+            }
+            message?:continue
+            isFirstReceive = false
+            logger.info { "receive: ${message.type} -  ${Json.encodeToString(message)}" }
+            return message
         }
-        isFirstReceive = false
-        logger.info { "receive: ${Json.encodeToString(message)}" }
-        return message
     }
 }
 
